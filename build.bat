e44c1b4d7a7e2861a417c6def9e497856dd53845
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2024-07-30 17:33:30 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ pyinstaller main.py -n "CustomDominicClientRpc" --onefile
 :: Для преобразования вашего скрипта в exe программу Вы можете использовать Pyinstaller
 :: To change python scripts in exe programs you can use Pyinstaller
 :: pip install pyinstaller
-:: (python from 3.8 to 3.12)
+:: (python from 3.8 to 3.13)
